cbb5b54aaf967c82276aba298d5449cbb3a93cbd
escolhadigital <joelrocha@escolhadigital.com>
2018-01-31 18:36:28 +0000

    B

--- wp-content/themes/astrid/includes/archives.php
+++
@@ -1,8 +0,0 @@
-<?php
-  
-  add_filter('getarchives_where', 'my_custom_post_type_archive_where', 10, 2);
-  function my_custom_post_type_archive_where($where,$args){  
-    $post_type  = isset($args['post_type'])  ? $args['post_type']  : 'post';  
-    $where = "WHERE post_type = '$post_type' AND post_status = 'publish'";
-    return $where;  
-  }
--- wp-content/themes/astrid/includes/feed.php
+++
@@ -1,4 +0,0 @@
-<?php 
-  // Add default posts and comments RSS feed links to head
-  add_theme_support('automatic-feed-links');
-?>
